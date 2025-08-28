ae767a713a9bef653793f97ffbfcf88e292c596c
Anant Kumar <adwayaanant@gmail.com>
2025-08-28 10:39:48 +0530

    responsive Nav bar

--- src/Component/Nav/Nav.jsx
+++ src/Component/Nav/Nav.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import './Nav.css'
 import {Link} from "react-scroll"
+
 const Nav = () => {
+  let menu = useRef()
+  let mobile = useRef()
+
+  const toggleMenu = () => {
+    mobile.current.classList.toggle("activemobile")
+    menu.current.classList.toggle("activeham")  
+  }
+
   return (
     <nav>
         <h1>PORTFOLIO</h1>
         <ul className='desktopmenu'>
-            <Link to="Home" smooth={true} activeClass='active' spy = {true} duration={500}><li>Home</li></Link>
-            <Link to="About" smooth={true} activeClass='active' spy = {true} duration={500}><li>About</li></Link>
-            <Link to="Projects" smooth={true} activeClass='active' spy = {true} duration={500}><li>Projects</li></Link>
-            <Link to="Contect" smooth={true} activeClass='active' spy = {true} duration={500}><li>Contect</li></Link>
-            
-            
+            <Link to="Home" smooth={true} activeClass='active' spy={true} duration={500}><li>Home</li></Link>
+            <Link to="About" smooth={true} activeClass='active' spy={true} duration={500}><li>About</li></Link>
+            <Link to="Projects" smooth={true} activeClass='active' spy={true} duration={500}><li>Projects</li></Link>
+            <Link to="Contact" smooth={true} activeClass='active' spy={true} duration={500}><li>Contact</li></Link>
         </ul>
-        <div className="hamburger">
+        <div className="hamburger" ref={menu} onClick={toggleMenu}>
           <div className="ham"></div>
           <div className="ham"></div>
           <div className="ham"></div>
         </div>
-        <ul className='mobilemenu'>
-            <Link to="Home" smooth={true} activeClass='active' spy = {true} duration={500}><li>Home</li></Link>
-            <Link to="About" smooth={true} activeClass='active' spy = {true} duration={500}><li>About</li></Link>
-            <Link to="Projects" smooth={true} activeClass='active' spy = {true} duration={500}><li>Projects</li></Link>
-            <Link to="Contect" smooth={true} activeClass='active' spy = {true} duration={500}><li>Contect</li></Link>
-            
-            
+
+        <ul className='mobilemenu' ref={mobile}>
+            <Link to="Home" smooth={true} activeClass='active' spy={true} duration={500} onClick={toggleMenu}><li>Home</li></Link>
+            <Link to="About" smooth={true} activeClass='active' spy={true} duration={500} onClick={toggleMenu}><li>About</li></Link>
+            <Link to="Projects" smooth={true} activeClass='active' spy={true} duration={500} onClick={toggleMenu}><li>Projects</li></Link>
+            <Link to="Contact" smooth={true} activeClass='active' spy={true} duration={500} onClick={toggleMenu}><li>Contact</li></Link>
         </ul>
     </nav>
   )
